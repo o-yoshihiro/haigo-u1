a16953d7b1381b06e8070e9b7bcf133d25549466
o-yoshihiro <yoshihiro.ogawa.mobile1@gmail.com>
2025-05-08 08:28:57 +0900

    Add files via upload

---
+++ HaigoForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HaigoForm() {
+  return (
+    <div>
+      <h1>配合表 入力UI（プロトタイプ）</h1>
+      <p>ここにフォームが表示されます。</p>
+    </div>
+  );
+}
+
+export default HaigoForm;
